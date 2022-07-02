7208647e8caa7dc871b99745fe514f93aa470bbf
JaewoongLee-swift <jaewoong0624@gmail.com>
2022-07-02 21:22:47 +0900

    rootViewController를 TabBarController로 변경, HomeViewController 생성 및 TabBarController에 연결

--- Toss/Toss/HomeViewController.swift
+++ Toss/Toss/HomeViewController.swift
@@ -1,16 +1,16 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Toss
 //
 //  Created by 이재웅 on 2022/07/02.
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
-
--- Toss/Toss/SceneDelegate.swift
+++ Toss/Toss/SceneDelegate.swift
@@ -11,15 +11,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         
         self.window = UIWindow(windowScene: windowScene)
-        window?.backgroundColor = .systemBackground
+        window?.backgroundColor = UIColor(red: 241/255, green: 243/255, blue: 245/255, alpha: 1.0)
         
-        self.window?.rootViewController = ViewController()
+        self.window?.rootViewController = TabBarController()
         self.window?.makeKeyAndVisible()
     }
 }
-
